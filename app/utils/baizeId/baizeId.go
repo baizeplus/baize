fc06b233cebb41a01973af444c789bc710379dd8
danny <dannyzhao0305@gmail.com>
2025-09-05 16:04:20 +0800

    修改id生成规则 使id最高可用2255年

--- app/utils/baizeId/baizeId.go
+++ app/utils/baizeId/baizeId.go
@@ -23,8 +23,8 @@ type Node struct {
 }
 
 func NewNode(node int32) error {
-	if node < 0 || node > 8191 {
-		return errors.New("node number must be between 0 and 8191")
+	if node < 0 || node > 4095 {
+		return errors.New("node number must be between 0 and 4095")
 	}
 	n = new(Node)
 	n.node = node
@@ -36,18 +36,18 @@ func GetId() string {
 	if n == nil {
 		var rByte = [5]byte{}
 		_, _ = io.ReadFull(r, rByte[:])
-		micro := time.Now().UnixMicro()
-		buf[0] = encode[(micro>>47)&0x1F]
-		buf[1] = encode[(micro>>42)&0x1F]
-		buf[2] = encode[(micro>>37)&0x1F]
-		buf[3] = encode[(micro>>32)&0x1F]
-		buf[4] = encode[(micro>>27)&0x1F]
-		buf[5] = encode[(micro>>22)&0x1F]
-		buf[6] = encode[(micro>>17)&0x1F]
-		buf[7] = encode[(micro>>12)&0x1F]
-		buf[8] = encode[(micro>>7)&0x1F]
-		buf[9] = encode[(micro>>2)&0x1F]
-		buf[10] = encode[(byte(micro<<3)|rByte[0]>>5)&0x1F]
+		now := time.Now().UnixMicro()
+		buf[0] = encode[(now>>48)&0x1F]
+		buf[1] = encode[(now>>43)&0x1F]
+		buf[2] = encode[(now>>38)&0x1F]
+		buf[3] = encode[(now>>33)&0x1F]
+		buf[4] = encode[(now>>28)&0x1F]
+		buf[5] = encode[(now>>23)&0x1F]
+		buf[6] = encode[(now>>18)&0x1F]
+		buf[7] = encode[(now>>13)&0x1F]
+		buf[8] = encode[(now>>8)&0x1F]
+		buf[9] = encode[(now>>3)&0x1F]
+		buf[10] = encode[(byte(now<<2)|rByte[0]>>5)&0x1F]
 		buf[11] = encode[rByte[0]&0x1F]
 		buf[12] = encode[rByte[1]&0x1F]
 		buf[13] = encode[rByte[2]&0x1F]
@@ -73,17 +73,17 @@ func GetId() string {
 	buf[14] = encode[n.step>>5&0x1F]
 	buf[15] = encode[n.step&0x1F]
 	n.mu.Unlock()
-	buf[0] = encode[(now>>47)&0x1F]
-	buf[1] = encode[(now>>42)&0x1F]
-	buf[2] = encode[(now>>37)&0x1F]
-	buf[3] = encode[(now>>32)&0x1F]
-	buf[4] = encode[(now>>27)&0x1F]
-	buf[5] = encode[(now>>22)&0x1F]
-	buf[6] = encode[(now>>17)&0x1F]
-	buf[7] = encode[(now>>12)&0x1F]
-	buf[8] = encode[(now>>7)&0x1F]
-	buf[9] = encode[(now>>2)&0x1F]
-	buf[10] = encode[((byte(now<<3))|byte(n.node>>10))&0x1F]
+	buf[0] = encode[(now>>48)&0x1F]
+	buf[1] = encode[(now>>43)&0x1F]
+	buf[2] = encode[(now>>38)&0x1F]
+	buf[3] = encode[(now>>33)&0x1F]
+	buf[4] = encode[(now>>28)&0x1F]
+	buf[5] = encode[(now>>23)&0x1F]
+	buf[6] = encode[(now>>18)&0x1F]
+	buf[7] = encode[(now>>13)&0x1F]
+	buf[8] = encode[(now>>8)&0x1F]
+	buf[9] = encode[(now>>3)&0x1F]
+	buf[10] = encode[((byte(now<<2))|byte(n.node>>10))&0x1F]
 	buf[11] = encode[(n.node>>5)&0x1F]
 	buf[12] = encode[(n.node)&0x1F]
 	return string(buf)
