e8f692e00b34782194b403fef43916ae0035e874
danny <dannyzhao0305@gmail.com>
2025-09-07 15:03:14 +0800

    修改定时任务的bug

--- sql/baize.sql
+++ sql/baize.sql
@@ -211,40 +211,44 @@ INSERT INTO `sys_dict_type` VALUES ('0000000000000010', '系统状态', 'sys_com
 -- Table structure for sys_job
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_job`;
-CREATE TABLE `sys_job`  (
-                            `job_id` char(16) NOT NULL COMMENT '任务ID',
-                            `job_name` varchar(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '' COMMENT '任务名称',
-                            `job_params` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL COMMENT '参数',
-                            `invoke_target` varchar(500) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL COMMENT '调用目标字符串',
-                            `cron_expression` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT '' COMMENT 'cron执行表达式',
-                            `status` char(1) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT '0' COMMENT '状态（0正常 1暂停）',
-                            `create_by` char(16) NULL DEFAULT NULL COMMENT '创建者',
-                            `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
-                            `update_by` char(16) NULL DEFAULT NULL COMMENT '更新者',
-                            `update_time` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
-                            PRIMARY KEY (`job_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_unicode_ci COMMENT = '定时任务调度表' ROW_FORMAT = Dynamic;
+CREATE TABLE `sys_job` (
+                           `job_id` char(16) COLLATE utf8_unicode_ci NOT NULL COMMENT '任务ID',
+                           `job_name` varchar(64) COLLATE utf8_unicode_ci NOT NULL DEFAULT '' COMMENT '任务名称',
+                           `job_params` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '参数',
+                           `invoke_target` varchar(500) COLLATE utf8_unicode_ci NOT NULL COMMENT '调用目标字符串',
+                           `cron_expression` varchar(255) COLLATE utf8_unicode_ci DEFAULT '' COMMENT 'cron执行表达式',
+                           `status` char(1) COLLATE utf8_unicode_ci DEFAULT '0' COMMENT '状态（0正常 1暂停）',
+                           `create_by` char(16) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '创建者',
+                           `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+                           `update_by` char(16) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '更新者',
+                           `update_time` datetime DEFAULT NULL COMMENT '更新时间',
+                           PRIMARY KEY (`job_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci ROW_FORMAT=DYNAMIC COMMENT='定时任务调度表';
 
 -- ----------------------------
 -- Records of sys_job
 -- ----------------------------
+BEGIN;
+INSERT INTO `sys_job` (`job_id`, `job_name`, `job_params`, `invoke_target`, `cron_expression`, `status`, `create_by`, `create_time`, `update_by`, `update_time`) VALUES ('67RBSK3TD4M01000', '无参调用', '', 'NoParams', '* * * * *', '0', '1', '2025-09-05 10:37:16', '0000000000000001', '2025-09-06 16:06:27');
+INSERT INTO `sys_job` (`job_id`, `job_name`, `job_params`, `invoke_target`, `cron_expression`, `status`, `create_by`, `create_time`, `update_by`, `update_time`) VALUES ('67RBSK3TD4M01001', '有参测试', 'baize,18', 'Params', '* * * * *', '1', '1', '2025-09-05 10:37:48', '0000000000000001', '2025-09-06 17:57:59');
+COMMIT;
 
 -- ----------------------------
 -- Table structure for sys_job_log
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_job_log`;
-CREATE TABLE `sys_job_log`  (
-                                `job_log_id` char(16) NOT NULL COMMENT '任务日志ID',
-                                `job_id` char(16) NOT NULL COMMENT '任务ID',
-                                `job_name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '任务名称',
-                                `job_group` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '任务组名',
-                                `invoke_target` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '调用目标字符串',
-                                `status` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '0' COMMENT '执行状态（0正常 1失败）',
-                                `exception_info` varchar(2000) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '' COMMENT '异常信息',
-                                `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
-                                `cost_time` bigint(20) NULL DEFAULT 0 COMMENT '耗时（毫秒）',
-                                PRIMARY KEY (`job_log_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '定时任务调度日志表' ROW_FORMAT = Dynamic;
+CREATE TABLE `sys_job_log` (
+                               `job_log_id` char(16) NOT NULL COMMENT '任务日志ID',
+                               `job_id` char(16) NOT NULL COMMENT '任务ID',
+                               `job_name` varchar(64) NOT NULL COMMENT '任务名称',
+                               `job_params` varchar(255) NOT NULL COMMENT '任务组名',
+                               `invoke_target` varchar(500) NOT NULL COMMENT '调用目标字符串',
+                               `status` char(1) DEFAULT '0' COMMENT '执行状态（0正常 1失败）',
+                               `exception_info` varchar(2000) DEFAULT '' COMMENT '异常信息',
+                               `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+                               `cost_time` bigint(20) DEFAULT '0' COMMENT '耗时（毫秒）',
+                               PRIMARY KEY (`job_log_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC COMMENT='定时任务调度日志表';
 
 -- ----------------------------
 -- Records of sys_job_log
